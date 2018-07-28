236e5ed6a91471949d9e670d740de0b646fb266b
Mateusz Kowalewski <Mateusz@localhost.localdomain>
2018-07-28 13:34:26 -0400

    commit

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -39,7 +39,8 @@
 						<div>
 						<ul class="tag success">
 							<li class="tag-elem">
-								<a href=" {{ route('tags.edit', ['id' => $tag->id ]) }}">{{ $tag->tag }}</a>
+								<!--<a href=" {{ route('tags.edit', ['id' => $tag->id ]) }}">{{ $tag->tag }}</a> -->
+								<a href="" data-toggle="modal" data-target="editmodal">{{ $tag->tag }}</a>
 								
 							</li>
 							<li class="tag-elem">
@@ -61,6 +62,31 @@
 		</table>
 		</div>	
 	</div>
+
+	<!-- modal -->
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+
+      		<div class="modal-header">
+	        	<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+	        	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          	<span aria-hidden="true">&times;</span>
+	        	</button>
+      		</div>
+
+     		<div class="modal-body">
+        
+      		</div>
+
+      		<div class="modal-footer">
+	        	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        	<button type="button" class="btn btn-primary">Save changes</button>
+	      	</div>
+    	</div>
+  	</div>
+</div>
 	
 	<script type="text/javascript">
 		$(document).ready(() =>{
